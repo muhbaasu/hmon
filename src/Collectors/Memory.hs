c06690db1ad9cfd473befffa50512f8daa5e9740
Marcel Buesing <buesing.marcel@googlemail.com>
2014-10-21 21:09:08 +0200

    Add optional memory size to memory collector.

--- src/Collectors/Memory.hs
+++ src/Collectors/Memory.hs
@@ -5,6 +5,8 @@ module Collectors.Memory where
 import Control.Lens.TH (makeClassy)
 import Text.Parsec
 import Text.Parsec.String
+import Control.Applicative (pure, (<*>), (<*), (*>))
+import Data.Functor (fmap, (<$>))
 
 data MemoryMetric = MemoryMetric { _metricName :: ! [Char]
                                  , _size :: !Int }
@@ -21,17 +23,9 @@ parseStat = do
 memoryLine :: Parser MemoryMetric
 memoryLine = do
   name <-  manyTill anyChar $ string ":"
-  _ <- many1 space
+  _ <- skipMany1 space
   memSize <- memorySize
   return $ MemoryMetric name memSize
 
 memorySize :: Parser Int
-memorySize = do
-  memSize <- many1 digit <?> "size"
-  return  $ read memSize
-
--- Fix this part which is optional, to make it actually work
---memEndLine = do
--- _ <- try option space string "kB"
--- _ <- newline
--- return ()
+memorySize =  read <$> (many1 digit) <* (optional $ string " kB")
